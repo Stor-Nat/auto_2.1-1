2642964ce45b72743936965277668ea3b16050b8
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-14 09:35:22 +0300

    Fixed bug in test

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -15,7 +15,7 @@ public class CallBackTest {
         form.$("[data-test-id=name] input").setValue("Анна-Мария");
         form.$("[data-test-id=phone] input").setValue("+79001234567");
         form.$("[data-test-id=agreement]").click();
-        form.$("[type=button").click();
+        $("[type=button").click();
 
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
