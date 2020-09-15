f4963d0cda70c63e12d478f5ad5b38e5425f7831
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-15 10:50:39 +0300

    Error correction after checking

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,7 +1,7 @@
-import com.codeborne.selenide.Configuration;
+package ru.netology;
+
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -10,16 +10,11 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CallBackTest {
 
-    @BeforeEach
-    void setUp() {
-        Configuration.headless = true;
-    }
-
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $("[id=root]");
-        form.$("[data-test-id=name] input").setValue("Анна-Мария");
+        form.$("[data-test-id=name] input").setValue("Анна Иванова");
         form.$("[data-test-id=phone] input").setValue("+79001234567");
         form.$("[data-test-id=agreement]").click();
         form.$("[type=button]").click();
